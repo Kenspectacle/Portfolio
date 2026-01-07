cb4100142e2fc51afbeff5a4e06ddf21195ed20e
Kenny Gozali <kennyftw2@gmail.com>
2026-01-07 15:32:59 +0100

    refactor: modularize the mainpage using <section>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import ProfileIcon from "@/ui/profile-icon"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-around p-24">
-        <ProfileIcon />
-        <IntroductionText />
+    <main>
+        <section className="flex min-h-screen items-center justify-around p-24">  
+          <ProfileIcon />
+          <IntroductionText />
+        </section>
     </main>
   )
 }
