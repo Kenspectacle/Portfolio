88fc8d22489c5feecf2bd90ea435ea398defbb6a
Kenny Gozali <kennyftw2@gmail.com>
2026-01-07 17:40:28 +0100

    refactor: removed unnecessary empty space

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,6 @@ export default function Home() {
               <span>
                 SOFTWARE ENGINEER
               </span>
-              
             </div>
         </section>
     </main>
