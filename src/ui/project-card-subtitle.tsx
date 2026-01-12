a1968ca6ae13476d9811e0749ea5fa365f8a54c6
Kenny Gozali <kennyftw2@gmail.com>
2026-01-12 15:44:16 +0100

    Style: changed the cards subtitle to red

--- src/ui/project-card-subtitle.tsx
+++ src/ui/project-card-subtitle.tsx
@@ -1,5 +1,5 @@
 export default function CardSubTitle ({ children }: {children: React.ReactNode})  {
     return (
-        <h6 className="mt-2 font-light text-gray-400">{children}</h6>
+        <h6 className="mt-2 font-light text-red-600">{children}</h6>
     )
 }
